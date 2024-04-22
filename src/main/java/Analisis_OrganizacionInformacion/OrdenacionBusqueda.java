817db60f0c8f7e743b52982627c0a6ab4f8ed976
ZoeAcero <zar.amazona@gmail.com>
2024-04-22 18:38:19 +0200

    a

--- src/main/java/Analisis_OrganizacionInformacion/OrdenacionBusqueda.java
+++ src/main/java/Analisis_OrganizacionInformacion/OrdenacionBusqueda.java
@@ -1,7 +1,6 @@
 package Analisis_OrganizacionInformacion;
 
-import Gestion_de_Datos_Dinamicos.Pareja;
-import Gestion_de_Datos_Dinamicos.ListaDatos;
+import Gestion_DatosDinamicos.*;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -16,17 +15,17 @@ public class OrdenacionBusqueda {
 
     // Método para ordenar los registros por el primer elemento de la pareja
     public void ordenarPorElemento1() {
-        Collections.sort(listaDatos.obtenerLista(), Comparator.comparing(Pareja::getElemento1));
+        Collections.sort(listaDatos.obtenerLista(), Comparator.comparing(Pair::getElemento1));
     }
 
     // Método para ordenar los registros por el segundo elemento de la pareja
     public void ordenarPorElemento2() {
-        Collections.sort(listaDatos.obtenerLista(), Comparator.comparing(Pareja::getElemento2));
+        Collections.sort(listaDatos.obtenerPairListGUI(), Comparator.comparing(Pair::getElemento2));
     }
 
     // Método para buscar una pareja por el primer elemento
-    public Pareja buscarPorElemento1(int elemento1) {
-        for (Pareja pareja : listaDatos.obtenerLista()) {
+    public Pair buscarPorElemento1(int elemento1) {
+        for (Pair pareja : listaDatos.obtenerLista()) {
             if (pareja.getElemento1() == elemento1) {
                 return pareja;
             }
@@ -34,8 +33,8 @@ public class OrdenacionBusqueda {
         return null;
     }
     // Método para buscar una pareja por el segundo elemento
-    public Pareja buscarPorElemento2(int elemento2) {
-        for (Pareja pareja : listaDatos.obtenerLista()) {
+    public Pair buscarPorElemento2(int elemento2) {
+        for (Pair pareja : listaDatos.obtenerLista()) {
             if (pareja.getElemento2() == elemento2) {
                 return pareja;
             }
