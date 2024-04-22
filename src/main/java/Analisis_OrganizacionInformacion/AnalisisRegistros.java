ea5057416338a776033ef5d1d1969ec1f36e152e
ZoeAcero <zar.amazona@gmail.com>
2024-04-22 18:54:18 +0200

    a

--- src/main/java/Analisis_OrganizacionInformacion/AnalisisRegistros.java
+++ src/main/java/Analisis_OrganizacionInformacion/AnalisisRegistros.java
@@ -3,4 +3,10 @@ package Analisis_OrganizacionInformacion;
 import Gestion_DatosDinamicos.*;
 
 public class AnalisisRegistros {
+    private PairListGUI listaDatos;
+
+    // Constructor
+    public AnalisisRegistros(PairListGUI listaDatos) {
+        this.listaDatos = listaDatos;
+    }
 }
