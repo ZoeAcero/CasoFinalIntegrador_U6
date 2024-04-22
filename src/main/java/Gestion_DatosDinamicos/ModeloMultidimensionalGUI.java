0875fe03988d1f3b0a133581dc2742f99c0e1d00
ZoeAcero <zar.amazona@gmail.com>
2024-04-22 12:24:54 +0200

    a

--- src/main/java/Gestion_DatosDinamicos/ModeloMultidimensionalGUI.java
+++ src/main/java/Gestion_DatosDinamicos/ModeloMultidimensionalGUI.java
@@ -1,6 +1,15 @@
 package Gestion_DatosDinamicos;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class ModeloMultidimensionalGUI extends JFrame {
-}
+    private final JTextField[][] camposTexto;
+    private final JPanel panelDatos;
+
+    public ModeloMultidimensionalGUI(int filas, int columnas) {
+        setTitle("Modelo Multidimensional");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
