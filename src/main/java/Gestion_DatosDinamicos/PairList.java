8b5705c65d95f422106708a15d09f7dc0214833c
ZoeAcero <zar.amazona@gmail.com>
2024-04-22 12:35:09 +0200

    a

--- src/main/java/Gestion_DatosDinamicos/PairList.java
+++ src/main/java/Gestion_DatosDinamicos/PairList.java
@@ -1,4 +1,20 @@
 package Gestion_DatosDinamicos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PairList {
+    private List<Pair> pairs;
+
+    public PairList() {
+        pairs = new ArrayList<>();
+    }
+
+    public void addPair(Pair pair) {
+        pairs.add(pair);
+    }
+
+    public Pair getPair(int index) {
+        return pairs.get(index);
+    }
 }
