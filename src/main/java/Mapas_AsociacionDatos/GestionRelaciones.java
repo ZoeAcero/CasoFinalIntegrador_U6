34b92f7f587ba5330da882d73d31bc0531436bd0
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 08:56:45 +0200

    a

--- src/main/java/Mapas_AsociacionDatos/GestionRelaciones.java
+++ src/main/java/Mapas_AsociacionDatos/GestionRelaciones.java
@@ -1,15 +1,54 @@
 package Mapas_AsociacionDatos;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
 import Gestion_DatosDinamicos.*;
 
 public class GestionRelaciones {
     private Map<Pair, Pair> relaciones;
+    private JFrame frame;
+    private JTextField input1;
+    private JTextField input2;
+    private JTextArea textArea;
 
     // Constructor
     public GestionRelaciones() {
         this.relaciones = new HashMap<>();
+        createAndShowGUI();
+    }
+
+    private void createAndShowGUI() {
+        frame = new JFrame("Gestion de Relaciones");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        input1 = new JTextField(10);
+        input2 = new JTextField(10);
+        JButton button = new JButton("Agregar Relacion");
+        textArea = new JTextArea(20, 30);
+        textArea.setEditable(false);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pair pareja1 = new Pair(Integer.parseInt(input1.getText()), Integer.parseInt(input2.getText()));
+                Pair pareja2 = new Pair(Integer.parseInt(input2.getText()), Integer.parseInt(input1.getText()));
+                agregarRelacion(pareja1, pareja2);
+                textArea.append(pareja1 + " -> " + pareja2 + "\n");
+            }
+        });
+
+        frame.add(input1);
+        frame.add(input2);
+        frame.add(button);
+        frame.add(new JScrollPane(textArea));
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public void agregarRelacion(Pair pareja1, Pair pareja2) {
@@ -23,5 +62,4 @@ public class GestionRelaciones {
     public void eliminarRelacion(Pair pareja) {
         this.relaciones.remove(pareja);
     }
-
 }
