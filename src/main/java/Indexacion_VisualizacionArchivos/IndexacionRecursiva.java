04181ac99cdec9568ea429df9a85128859f050c7
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 09:09:37 +0200

    a

---
+++ src/main/java/Indexacion_VisualizacionArchivos/IndexacionRecursiva.java
@@ -0,0 +1,15 @@
+package Indexacion_VisualizacionArchivos;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexacionRecursiva {
+    private List<File> archivosIndexados;
+    private JFrame frame;
+    private JTextArea textArea;
+}
