9493010567eaa24565e824cccfd6499703a59af3
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 09:06:17 +0200

    a

--- src/main/java/Indexacion_VisualizacionArchivos/OrdenacionListado.java
+++ src/main/java/Indexacion_VisualizacionArchivos/OrdenacionListado.java
@@ -1,4 +1,16 @@
 package Indexacion_VisualizacionArchivos;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class OrdenacionListado {
+    private List<File> archivosIndexados;
+    private JFrame frame;
+    private JTextArea textArea;
 }
