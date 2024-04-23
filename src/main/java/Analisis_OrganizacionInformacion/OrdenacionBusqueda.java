43413b155aa05e5d03212da62856c11c434d52ef
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 09:02:52 +0200

    a

--- src/main/java/Analisis_OrganizacionInformacion/OrdenacionBusqueda.java
+++ src/main/java/Analisis_OrganizacionInformacion/OrdenacionBusqueda.java
@@ -1,5 +1,9 @@
 package Analisis_OrganizacionInformacion;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import Gestion_DatosDinamicos.*;
 
 import java.util.Collections;
@@ -7,10 +11,70 @@ import java.util.Comparator;
 
 public class OrdenacionBusqueda {
     private PairListGUI listaDatos;
+    private JFrame frame;
+    private JTextField input;
+    private JTextArea textArea;
 
     // Constructor
     public OrdenacionBusqueda(PairListGUI listaDatos) {
         this.listaDatos = listaDatos;
+        createAndShowGUI();
+    }
+
+    private void createAndShowGUI() {
+        frame = new JFrame("Ordenacion y Busqueda");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        input = new JTextField(10);
+        JButton sortFirstButton = new JButton("Ordenar por First");
+        JButton sortSecondButton = new JButton("Ordenar por Second");
+        JButton searchFirstButton = new JButton("Buscar por First");
+        JButton searchSecondButton = new JButton("Buscar por Second");
+        textArea = new JTextArea(20, 30);
+        textArea.setEditable(false);
+
+        sortFirstButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ordenarPorFirst();
+                textArea.append("Registros ordenados por First.\n");
+            }
+        });
+
+        sortSecondButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ordenarPorSecond();
+                textArea.append("Registros ordenados por Second.\n");
+            }
+        });
+
+        searchFirstButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pair result = buscarPorFirst(Integer.parseInt(input.getText()));
+                textArea.append("Resultado de la búsqueda por First: " + result + "\n");
+            }
+        });
+
+        searchSecondButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pair result = buscarPorSecond(Integer.parseInt(input.getText()));
+                textArea.append("Resultado de la búsqueda por Second: " + result + "\n");
+            }
+        });
+
+        frame.add(input);
+        frame.add(sortFirstButton);
+        frame.add(sortSecondButton);
+        frame.add(searchFirstButton);
+        frame.add(searchSecondButton);
+        frame.add(new JScrollPane(textArea));
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     // Método para ordenar los registros por el primer elemento de la pareja
