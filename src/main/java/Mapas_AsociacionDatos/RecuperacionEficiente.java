82bf1bb79509a7caa6cce11900e15191beb55e01
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 08:58:28 +0200

    a

--- src/main/java/Mapas_AsociacionDatos/RecuperacionEficiente.java
+++ src/main/java/Mapas_AsociacionDatos/RecuperacionEficiente.java
@@ -1,14 +1,63 @@
 package Mapas_AsociacionDatos;
 
-import Gestion_DatosDinamicos.Pair;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Map;
+import Gestion_DatosDinamicos.*;
 
 public class RecuperacionEficiente {
     private Map<Pair, Pair> relaciones;
+    private JFrame frame;
+    private JTextField input1;
+    private JTextField input2;
+    private JTextArea textArea;
 
     // Constructor
     public RecuperacionEficiente(Map<Pair, Pair> relaciones) {
         this.relaciones = relaciones;
+        createAndShowGUI();
+    }
+
+    private void createAndShowGUI() {
+        frame = new JFrame("Recuperacion Eficiente");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        input1 = new JTextField(10);
+        input2 = new JTextField(10);
+        JButton retrieveButton = new JButton("Obtener Relacion");
+        JButton deleteButton = new JButton("Eliminar Relacion");
+        textArea = new JTextArea(20, 30);
+        textArea.setEditable(false);
+
+        retrieveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pair pareja = new Pair(Integer.parseInt(input1.getText()), Integer.parseInt(input2.getText()));
+                Pair relacion = obtenerRelacion(pareja);
+                textArea.append(pareja + " -> " + relacion + "\n");
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pair pareja = new Pair(Integer.parseInt(input1.getText()), Integer.parseInt(input2.getText()));
+                eliminarRelacion(pareja);
+                textArea.append("Relacion " + pareja + " eliminada.\n");
+            }
+        });
+
+        frame.add(input1);
+        frame.add(input2);
+        frame.add(retrieveButton);
+        frame.add(deleteButton);
+        frame.add(new JScrollPane(textArea));
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public Pair obtenerRelacion(Pair pareja) {
