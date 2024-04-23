50276cebf3f9a06bd5e417c68a5f6d3cf5aef03a
ZoeAcero <zar.amazona@gmail.com>
2024-04-23 08:55:12 +0200

    a

--- src/main/java/Mapas_AsociacionDatos/RecuperacionEficiente.java
+++ src/main/java/Mapas_AsociacionDatos/RecuperacionEficiente.java
@@ -4,5 +4,18 @@ import Gestion_DatosDinamicos.Pair;
 import java.util.Map;
 
 public class RecuperacionEficiente {
-    
+    private Map<Pair, Pair> relaciones;
+
+    // Constructor
+    public RecuperacionEficiente(Map<Pair, Pair> relaciones) {
+        this.relaciones = relaciones;
+    }
+
+    public Pair obtenerRelacion(Pair pareja) {
+        return this.relaciones.get(pareja);
+    }
+
+    public void eliminarRelacion(Pair pareja) {
+        this.relaciones.remove(pareja);
+    }
 }
